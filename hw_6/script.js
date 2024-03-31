aaa5e6f4e6ae4d7f6a6cc9e0537ff3bd01555828
viktoriiatarasenko <victoriataracenko0412@gmail.com>
2024-03-31 15:10:43 +0300

    1-3 done

---
+++ hw_6/script.js
@@ -0,0 +1,104 @@
+// Literals
+// {
+//     const cat = {
+//         name: "Кузя",
+//         age: 4,
+//         neutered: true
+//    }
+// }
+// Literals expand
+// {
+//     const key1 = prompt("Введіть назву першого ключа");
+//     const value1 = prompt("Введіть значення першого ключа");
+//     const key2 = prompt("Введіть назву другого ключа");
+//     const value2 = prompt("Введіть значення другого ключа");
+
+//     const cat = {
+//         name: "Кузя",
+//         age: 4,
+//         neutered: true,
+//         [key1]: value1,
+//         [key2]: value2
+//    }
+//    console.log(cat)
+// }
+// Literals copy
+// {
+//     const key1 = prompt("Введіть назву першого ключа");
+//     const value1 = prompt("Введіть значення першого ключа");
+//     const key2 = prompt("Введіть назву другого ключа");
+//     const value2 = prompt("Введіть значення другого ключа");
+
+//     const cat = {
+//         name: "Кузя",
+//         age: 4,
+//         neutered: true,
+//         [key1]: value1,
+//         [key2]: value2,
+//    }
+//    const key3 = prompt("Введіть назву третьго ключа");
+//    const value3 = prompt("Введіть значення третьго ключа");
+
+//    const cat2 = {
+//         ...cat,
+//         [key3]: value3
+//    }
+
+//    console.log(cat2)
+// }
+// Html tree
+{
+
+}
+// Parent
+{
+
+}
+// Change OK
+{
+
+}
+// Destructure
+{
+
+}
+// Destruct array
+{
+
+}
+// Destruct string
+{
+
+}
+// Destruct 2
+{
+
+}
+// Destruct 3
+{
+
+}
+// Copy delete
+{
+
+}
+// Currency real rate 
+{
+
+}
+// Currency drop down
+{
+
+}
+// Currency table
+{
+
+}
+// Form
+{
+
+}
+// Table
+{
+
+}
