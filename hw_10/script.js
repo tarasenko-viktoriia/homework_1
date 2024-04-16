8e40124b34337f852f66f524596ac0ffbfb60ce9
victoriataracenko0412 <victoriataracenko0412@gmail.com>
2024-04-16 13:24:26 +0300

    done hw

--- hw_10/script.js
+++ hw_10/script.js
@@ -1,78 +1,191 @@
 //while confirm
-// while (!confirm("Ви втомились?")) {};
+while (!confirm("Ви втомились?")) {};
 
 //array fill
-// {
-//     let arr= [];
-//     let prompt;
-//     while ((prompt = prompt("Введіть щось "))) {
-//     arr.push(prompt);
-//     console.log(arr);
-//     }
-// }
+{
+  let arr = [];
+  while (true) {
+      let userInput = prompt("Введіть елемент масиву:");
+      if (userInput === null) {
+          break;
+      } else {
+          arr.push(userInput);
+      }
+  }
+  console.log("Отриманий масив:", arr);  
+}
 
 //array fill nopush
-// const arr = [];
-// let index = 0;
-// while (true) {
-//   const answer = prompt("Введіть щось ");
-//   if (answer) {
-//     arr[index] = answer;
-//     index++;
-//   } else {
-//     break;
-//   }
-// }
-// console.log(arr);
+{
+  const arr = [];
+  let index = 0;
+  while (true) {
+    const answer = prompt("Введіть щось ");
+    if (answer) {
+      arr[index] = answer;
+      index++;
+    } else {
+      break;
+    }
+  }
+  console.log(arr);
+}
 
 //infinite probability
-// let index = 0;
-// while (true) {
-//     const num = Math.random();
-//     if (num > 0.9) {
-//       break;
-//     }
-//     index++
-// }
-// console.log(`Кількість ітерацій: ${index}`)
+let index = 0;
+while (true) {
+    const num = Math.random();
+    if (num > 0.9) {
+      break;
+    }
+    index++
+}
+console.log(`Кількість ітерацій: ${index}`)
 
 //empty loop
-// while (!prompt()) {};
+while (!prompt()) {};
 
 //progression sum
-// const n = +prompt();
-// let sum = 0;
-// for (let i = 0; i <= n; i += 3) {
-//   sum += i;
-//   console.log(i);
-// }
-
-//chess one line (ДОРОБИТИ!!!!)
-let i = ".";
-let e = "#"
-let str = "";
-let length = 7;
-for (let j=0; j < length; j++) {
-  str += i + e;
+const n = +prompt();
+let sum = 0;
+for (let i = 0; i <= n; i += 3) {
+  sum += i;
+  console.log(i);
 }
-console.log(str)
+
+//chess one line
+{
+  let i = ".";
+  let e = "#";
+  let str = "";
+  let length = 8;
+  for (let j = 0; j < length / 2; j++) {
+    str += i + e;
+  }
+  if (length % 2 === 1) {
+    str = str.slice(0, -1);
+  }
+  console.log(str);
+}
 
 //numbers
-
-// let str = "";
-// for (let i = 0; i < 10; i++) {
-//   for (let j = 0; j < 10; j++) {
-//     str += j;
-//   }
-//   str += "\n";
-// }
-// console.log(str);
+{
+  let str = "";
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      str += j;
+    }
+    str += "\n";
+  }
+  console.log(str);  
+}
 
 //chess
+  let i = ".";
+  let e = "#";
+  let str = "";
+  let length = 12;
+  let rows = 10;
+  for (let row = 0; row < rows; row++) {
+    for (let j = 0; j < length / 2; j++) {
+      str += i + e;
+    }
+    if (length % 2 === 1) {
+      str = str.slice(0, -1);
+    }
+    str += "\n";
+  }
+  console.log(str);
+
 //cubes
+{
+  let n = parseInt(prompt("Введіть кількість елементів масиву:"));
+  let arr = [];
+  for (let i = 0; i < n; i++) {
+      arr.push(i ** 3);
+  }
+  console.log("Масив кубів індексів:", arr);
+}
+
 //multiply table
+let arr = [];
+for (let i = 1; i <= 10; i++) {
+    arr[i] = []; 
+    for (let j = 1; j <= 10; j++) {
+        arr[i][j] = i * j; 
+    }
+}
+let result = arr[2][6]; 
+console.log(result);
+
 //read array of objects
+  const readArrayOfObjects = () => {
+    let arrayOfObjects = [];
+    while (true) {
+        let object = {};
+        let continueInput = true;
+        while (true) {
+            let key = prompt("Введіть ключ об'єкта:");
+            if (key === null) 
+            break;
+            let value = prompt("Введіть значення об'єкта:");
+            if (value === null) 
+            break;
+            object[key] = value;
+            continueInput = confirm("Бажаєте додати ще ключі та значення до цього об'єкта?");
+            if (!continueInput) 
+            break;
+        }
+        arrayOfObjects.push(object);
+        let continueAdding = confirm("Бажаєте додати ще об'єкти до масиву?");
+        if (!continueAdding) 
+        break;
+    }
+    return arrayOfObjects;
+  }
+  console.log("Масив об'єктів:", readArrayOfObjects);
+
 //Ромбік
+  const size = +prompt();
+
+  const halfRow = Math.floor(size / 2);
+  let dotsLength = halfRow;
+  let cagesLength = 1;
+
+  for (let i = 0; i < halfRow; i++) {
+    const dots = "".padStart(dotsLength, ".");
+    const cage = "".padStart(cagesLength, "#");
+    console.log(dots + cage + dots);
+    cagesLength += 2;
+    dotsLength--;
+  }
+
+  for (let j = 0; j < halfRow + 1; j++) {
+    const dots = "".padStart(dotsLength, ".");
+    const cage = "".padStart(cagesLength, "#");
+    console.log(dots + cage + dots);
+    cagesLength -= 2;
+    dotsLength++;
+  }
+
 //DOM: multiply table
-//DOM: highlight cell
-//DOM: Highlight cross
+  let table = document.createElement('table');
+  let tbody = document.createElement('tbody');
+
+  for (let i = 1; i <= 10; i++) {
+      let row = document.createElement('tr');
+      for (let j = 1; j <= 10; j++) {
+          let cell = document.createElement('td');
+          cell.innerText = i * j;
+          cell.addEventListener('mouseover', function() { //DOM: highlight cell
+            this.style.backgroundColor = 'pink';
+          });
+          cell.addEventListener('mouseout', function() {//DOM: highlight cell
+              this.style.backgroundColor = '';
+          });
+          row.append(cell);
+      }
+      tbody.append(row);
+  }
+  table.append(tbody);
+  document.body.append(table);
